07ef1baa7687b8f73af6d1ed5ece8e4a1c91bc73
xinlin <xinlin.zhang@hotmail.com>
2023-12-07 19:14:37 +0800

    init

--- README.md
+++ README.md
@@ -1,1 +1,56 @@
 # tcping.py
+
+```shell
+$ python tcping.py -h
+usage: tcping.py [-h] [-V] [-n N] [-i I] [-t T] host port
+
+positional arguments:
+  host           ip or FQDN
+  port           port number
+
+options:
+  -h, --help     show this help message and exit
+  -V, --version  show program's version number and exit
+  -n N           tcping count, 0 means infinity
+  -i I           tcping interval in second
+  -t T           tcping timeout in second
+$
+$ python tcping.py 1.1.1.1 443 -n8 -t0.5 -i0.2
+# tcping by connect (tcping.py)
+# ip: ['1.1.1.1'] , port: 443
+# count: 8
+# interval: 0.2s
+# timeout: 0.5s
+# C:Connection time, M:Mean, Md:Median, J:Jitter
+# Time Unit: millisecond
+# Mean&Median&Jitter: calc the last(max) **64** valid measures
+tcping 1.1.1.1:443
+[2023-12-07 19:11:07.513 tcping 1.1.1.1:443 1/1] 206.823(C)  206.823(M)  206.823(Md) 0(J)
+[2023-12-07 19:11:07.932 tcping 1.1.1.1:443 2/2] 218.219(C)  212.521(M)  212.521(Md) 8.058(J)
+[2023-12-07 19:11:08.353 tcping 1.1.1.1:443 3/3] 219.524(C)  214.856(M)  218.219(Md) 6.987(J)
+[2023-12-07 19:11:08.758 tcping 1.1.1.1:443 4/4] 204.114(C)  212.17(M)   212.521(Md) 7.835(J)
+[2023-12-07 19:11:09.148 tcping 1.1.1.1:443 5/5] 190.142(C)  207.765(M)  206.823(Md) 11.962(J)
+[2023-12-07 19:11:09.546 tcping 1.1.1.1:443 6/6] 197.272(C)  206.016(M)  205.469(Md) 11.525(J)
+[2023-12-07 19:11:09.967 tcping 1.1.1.1:443 7/7] 219.754(C)  207.979(M)  206.823(Md) 11.732(J)
+[2023-12-07 19:11:10.366 tcping 1.1.1.1:443 8/8] 198.777(C)  206.828(M)  205.469(Md) 11.339(J)
+$
+$ python tcping.py bing.com 443 -n4 -t0.1 -i0.1
+# tcping by connect (tcping.py)
+# ip: ['204.79.197.200', '13.107.21.200'] , port: 443
+# count: 4
+# interval: 0.1s
+# timeout: 0.1s
+# C:Connection time, M:Mean, Md:Median, J:Jitter
+# Time Unit: millisecond
+# Mean&Median&Jitter: calc the last(max) **64** valid measures
+tcping 204.79.197.200:443
+[2023-12-07 19:11:33.664 tcping 204.79.197.200:443 1/1] 69.196(C)   69.196(M)   69.196(Md)  0(J)
+[2023-12-07 19:11:33.830 tcping 204.79.197.200:443 2/2] 65.853(C)   67.525(M)   67.525(Md)  2.364(J)
+[2023-12-07 19:11:34.005 tcping 204.79.197.200:443 3/3] 74.012(C)   69.687(M)   69.196(Md)  4.101(J)
+[2023-12-07 19:11:34.169 tcping 204.79.197.200:443 4/4] 62.814(C)   67.969(M)   67.525(Md)  4.798(J)
+tcping 13.107.21.200:443
+[2023-12-07 19:11:34.236 tcping 13.107.21.200:443 1/1] 65.648(C)   65.648(M)   65.648(Md)  0(J)
+[2023-12-07 19:11:34.375 tcping 13.107.21.200:443 2/2] 38.151(C)   51.899(M)   51.899(Md)  19.444(J)
+[2023-12-07 19:11:34.541 tcping 13.107.21.200:443 3/3] 65.581(C)   56.46(M)    65.581(Md)  15.856(J)
+[2023-12-07 19:11:34.679 tcping 13.107.21.200:443 4/4] 37.231(C)   51.653(M)   51.866(Md)  16.126(J)
+```
---
+++ tcping.py
@@ -0,0 +1,119 @@
+#!/usr/bin/env python3
+"""
+tcping by connect in Python
+
+Author:   xinlin-z
+Github:   https://github.com/xinlin-z/pingtools
+Blog:     https://cs.pynote.net
+License:  MIT
+"""
+import socket
+from time import sleep
+from time import monotonic as time
+from datetime import datetime
+import argparse
+import math
+
+
+def mean(lst):
+    assert len(lst) >= 1
+    return round(sum(lst)/len(lst), 3)
+
+
+def median(lst):
+    assert len(lst) >= 1
+    a = lst[:]
+    n = len(a)
+    m = n // 2
+    a.sort()
+    md = a[m] if n%2 else (a[m-1]+a[m])/2
+    return round(md, 3)
+
+
+def std(lst):
+    assert len(lst) >= 1
+    if (n:=len(lst)) == 1:
+        return 0
+    m = mean(lst)
+    var = sum((i-m)**2 for i in lst) / (n-1)
+    return round(math.sqrt(var), 3)
+
+
+def stralign(s):
+    return s.ljust(11, ' ')
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-V', '--version', action='version',
+        version='tcping by xinlin-z (https://github.com/xinlin-z/tcping.py)')
+    parser.add_argument('host', help='ip or FQDN')
+    parser.add_argument('port', type=int, help='port number')
+    parser.add_argument('-n', type=int, default=4,
+                        help='tcping count, 0 means infinity')
+    parser.add_argument('-i', type=float, default=0.5,
+                        help='tcping interval in second')
+    parser.add_argument('-t', type=float, default=3.0,
+                        help='tcping timeout in second')
+    args = parser.parse_args()
+
+    ips = [t[4][0] for t in socket.getaddrinfo(args.host.strip(),
+                                               None,
+                                               socket.AF_INET,
+                                               socket.SOCK_STREAM)]
+    if args.n < 0:
+        args.n = 4
+    if args.i < 0:
+        args.i = 0.5
+    if args.t < 0:
+        args.t = 3.0
+
+    print('# tcping by connect (tcping.py)')
+    print('# ip:', str(ips), ', port:', args.port)
+    print('# count:', args.n if args.n else 'infinity')
+    print('# interval:', str(args.i)+'s')
+    print('# timeout:', str(args.t)+'s')
+    print('# C:Connection time, M:Mean, Md:Median, J:Jitter')
+    print('# Time Unit: millisecond')
+    print('# Mean&Median&Jitter: calc the last(max) **64** valid measures')
+    for ip in ips:
+        print('tcping', ip+':'+str(args.port))
+        i = 0
+        j = 0
+        ct = []
+        while True:
+            try:
+                success = False
+                s = socket.socket()
+                s.settimeout(args.t)
+                tic = time()
+                s.connect((ip,args.port))
+                toc = time()
+                s.close()
+                success = True
+                j += 1
+                ct.append((toc-tic)*1000)
+                ct = ct[-64:]
+                conntime = str(round(ct[-1],3))+'(C)'
+            except ConnectionRefusedError:
+                msg = 'connection refused'
+            except socket.timeout:
+                msg = 'timeout(>%sms)' % str(round(args.t*1000))
+
+            i += 1
+            ts = datetime.now().strftime('%Y-%m-%d %H:%M:%S.%f')[:-3]
+            show = f'[{ts} tcping {ip}:{args.port} {j}/{i}]'
+            if success:
+                print(show,
+                      stralign(conntime),
+                      stralign(str(mean(ct))+'(M)'),
+                      stralign(str(median(ct))+'(Md)'),
+                      stralign(str(std(ct))+'(J)'))
+            else:
+                print(show, msg)
+
+            if i != args.n:
+                sleep(args.i)
+            else:
+                break
+
